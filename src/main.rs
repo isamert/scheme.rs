c9cdf53af2a1418217fb0c7639d590f691e9cd4e
İsa Mert Gürbüz <isamertgurbuz@gmail.com>
2018-06-16 00:30:37 +0300

    add very simple repl

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,19 @@
 #[macro_use]
 mod util;
+
 mod env;
 mod lexer;
 mod parser;
 mod procedure;
 mod evaluator;
 mod primitives;
+mod pretty_print;
+mod repl;
 
-use parser::SExpr;
 use env::Env;
-use env::EnvRefT;
 
 fn main() {
-    let tokens = lexer::tokenize("
-(define a (list a b c d e f 1 2 3 4))
-(car a)
-(cdr a)
-(car (cdr (cdr (cdr a))))
-    ");
-
-    //println!("{:#?}", tokens);
-    let sexprs = parser::parse(tokens);
-    let globalenv = Env::with_values(Env::null(), primitives::env());
-    let globalenv_ref = globalenv.to_ref();
-
-    for (i, sexpr) in sexprs.iter().enumerate() {
-        //println!("{:#?}", sexpr);
-        let evaluated = evaluator::eval(&sexpr, globalenv_ref.clone_ref());
-        if let SExpr::Procedure(_) = evaluated {
-            println!("${}: <procedure>", i);
-        } else {
-            println!("${}: {:#?}", i, evaluated);
-        }
-    }
+    let env = Env::with_values(Env::null(), primitives::env());
+    repl::run(env.to_ref());
 }
 
---
+++ src/repl.rs
@@ -0,0 +1,36 @@
+use std::io;
+use std::io::prelude::*;
+
+use lexer;
+use lexer::Token;
+use parser;
+use parser::SExpr;
+use evaluator;
+use evaluator::Args;
+use env::EnvRef;
+use env::EnvRefT;
+use primitives::lang;
+
+pub fn run(env: EnvRef) {
+    let stdin = io::stdin();
+
+    let mut i = 0;
+    for line in stdin.lock().lines() {
+        let sexprs = parser::parse(lexer::tokenize(&line.unwrap()));
+        for sexpr in sexprs.iter() {
+            let evaluated = evaluator::eval(sexpr, env.clone_ref());
+            println!("${} = {}", i, evaluated);
+
+            // TODO: create an `args!` macro
+            // and maybe create SExpr::symbol(str), SExpr::float(f64)...
+
+            // Add $i to environment so user can use the currently evaluated value
+            lang::define(Args::new(
+                vec![SExpr::Atom(Token::Symbol(format!("${}", i))), evaluated],
+                env.clone_ref()
+            ));
+
+            i += 1;
+        }
+    }
+}
