f0f4133b16ecf0f98314af1175b10b414bc4dba9
Filip Filmar <fmil@google.com>
2020-07-25 00:51:20 -0700

    Added lib.rs.
    This allows reuse and embedding of the interpreter.

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+#[macro_use]
+pub mod serr;
+#[macro_use]
+pub mod utils;
+
+pub mod env;
+pub mod lexer;
+pub mod parser;
+pub mod expander;
+pub mod port;
+pub mod procedure;
+pub mod evaluator;
+pub mod primitives;
+pub mod pretty_print;
+pub mod repl;
+
